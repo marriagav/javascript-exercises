2f602e591198f72bed1be66a1953309d25a68e42
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-12-24 14:30:44 -0600

    palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const skip = { "!": 0, ".": 0, "?": 0, ",": 0, " ": 0 };
+  word = word.toLowerCase();
+  let left = 0;
+  let right = word.length - 1;
+  while (left < right) {
+    if (word[right] in skip) {
+      right--;
+      continue;
+    }
+    if (word[left] in skip) {
+      left++;
+      continue;
+    }
+    console.log(word[left], word[right]);
+    if (word[left] != word[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 };
 
+// palindromes("racecar");
 // Do not edit below this line
 module.exports = palindromes;
