5d5d0ab410071f2b9fe927973061347e30b29154
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-12-24 14:35:33 -0600

    fibo done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (fibo) {
+  if (fibo < 0) {
+    return "OOPS";
+  }
+  map = { 0: 1, 1: 1, 2: 1 };
+  return fiboRecursive(fibo, map);
 };
+
+function fiboRecursive(fibo, map) {
+  if (fibo in map) {
+    return map[fibo];
+  } else {
+    map[fibo] = fiboRecursive(fibo - 1, map) + fiboRecursive(fibo - 2, map);
+    return map[fibo];
+  }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
