7e6bd4205d5fc789600a9dffc1f206af249e877b
Miguel Arriaga Velasco <marriagav3@gmail.com>
2022-12-24 14:55:48 -0600

    oldest done:

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const oldest = people.reduce((older, current) => {
+    const olderAge = getAge(older);
+    const youngerAge = getAge(current);
+    return olderAge > youngerAge ? older : current;
+  }, {});
+  return oldest;
 };
+
+function getAge(person) {
+  if (person == {} || person == undefined) {
+    return 0;
+  }
+  const birth = person["yearOfBirth"];
+  let death = 0;
+  if (!("yearOfDeath" in person)) {
+    death = new Date().getFullYear();
+  } else {
+    death = person["yearOfDeath"];
+  }
+  return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
